deba0a58fcdd174bdc5f8ff8ad88667964f82c14
P4NK4J <pankajkandpal001@gmail.com>
2020-02-18 00:08:35 +0530

    recent changes

--- views/authentication/reset.error.view.php
+++ views/authentication/reset.error.view.php
@@ -5,7 +5,7 @@ if (session_status() == PHP_SESSION_NONE) {
 
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     if ($_SESSION['user_type'] === 'reader') {
-        header("location: /user");
+        header("location: /reader");
         exit;
     } elseif ($_SESSION['user_type'] === 'admin') {
         header("location: /admin");
