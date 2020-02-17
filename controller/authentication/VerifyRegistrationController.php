7a7549527cf99e59cab5248f62eb4303a5135313
P4NK4J <pankajkandpal001@gmail.com>
2020-02-17 23:41:26 +0530

    recent changes

--- controller/authentication/VerifyRegistrationController.php
+++ controller/authentication/VerifyRegistrationController.php
@@ -4,13 +4,11 @@ $hash = $_GET['hash'];
 
 
 $results = $app['database']->activateUser($hash);
-var_dump($results);
-die(); 
+ 
 if (isset($results)) {
 
-    header("location/verified");
-
-    
+    header("location:/verified");
+   
 } else {
     echo "Please verify your account";
 }
