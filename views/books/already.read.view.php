be4202acc94b49eccf22d659950f32c031ad0777
P4NK4J <pankajkandpal001@gmail.com>
2020-02-20 20:02:35 +0530

    recent changes

--- views/books/already.read.view.php
+++ views/books/already.read.view.php
@@ -58,7 +58,7 @@ $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </script>
 
 <body style="background-color: rgba(101,157,189,0.4);">
-    <div id="content" class="p-4 p-md-5 pt-5 h-100" style="min-height:90%;padding-right:6rem; font-family: 'Open Sans', sans-serif;">
+    <div id="content" class="p-4 p-md-5 pt-5 min-vh-100" style="padding-right:6rem; font-family: 'Open Sans', sans-serif;">
         <div class="searchbar mr-4" style="float: right; max-width:100%;">
             <input class="search_input" type="text" placeholder="Search..." id="myFilter">
             <a href="#" class="search_icon"><i class="fa fa-search"></i></a>
